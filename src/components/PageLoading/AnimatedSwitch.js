9763db4226b94c3e1f28d5fce6df5f66e0cd9dc0
xiaojun <Xj(%_2017>
2019-09-24 16:39:56 +0800

    菜单切换动画-all

--- src/components/PageLoading/AnimatedSwitch.js
+++ src/components/PageLoading/AnimatedSwitch.js
@@ -1,7 +1,8 @@
 import React from 'react'
-import { TransitionGroup, CSSTransition } from 'react-transition-group'
+import { SwitchTransition,TransitionGroup, CSSTransition } from 'react-transition-group'
 import { Route, Switch } from 'react-router-dom'
 import { connect } from 'dva';
+import { Spin } from 'antd';
 import styles from './style.less';
 
 const { useState } =React;
@@ -9,71 +10,77 @@ const AnimatedSwitch = props => {
   const [ show, setShow] = useState(false);
 
   const { children, loading } = props;
+  console.log(show);
   return (
     <div className={styles.route}>
-      <Route
-        render={({ location }) => (
-          <TransitionGroup>
-            <CSSTransition
-              in={loading}
-              timeout={{
-                enter: 600,
-                exit: 600,
-              }}
-              key={location.key}
-              classNames='fade'
-              className={styles.fade}
-              exit
-              onEnter={(node, isAppearing)=>{
-                console.log(show);
-                console.log('node');
-                console.log('开始进入。。。');
-                console.log(node)
-                console.log(isAppearing)
-                console.log('isAppearing')
-             }}
-              onEntering={(node, isAppearing) => {
-                console.log(show);
-                console.log('进入中。。。');
-                console.log('entering11')
-                console.log('onEntering-node')
-                console.log(node)
-                console.log(isAppearing)
-              }}
-              onExit={(node) => {
-                console.log(show);
-                console.log(node);
-                console.log('退出之前')
-                console.log('onExiting？？？')
-              }}
-              onExiting={(node) => {
-                console.log(show);
-                console.log(node);
-                console.log('退出中....')
-                console.log('onExiting222')
-              }}
-              onEntered={(node, isAppearing) => {
-                setShow(false)
-                console.log(show);
-                console.log('进入完成....')
-                console.log(node,isAppearing)
-                console.log('onEntered3333')
-              }}
-              onExited={(node) => {
-                console.log(show);
-                console.log(node);
-                console.log('退出完成....')
-                console.log('onExited444')
-                setShow(true)
-              }}
-            >
-              <Switch location={location}>{children}</Switch>
-            </CSSTransition>
-          </TransitionGroup>
-        )}
-      />
-    </div>
+      <Spin
+        spinning={show}
+        size="large"
+        style={{margin: 'auto', width: '100%'}}
+      >
+        <Route
+          render={({ location,match }) => (
+            <SwitchTransition>
+              <CSSTransition
+                appear
+                in={show}
+                timeout={300}
+                key={location.key}
+                classNames='fade'
+                unmountOnExit
+                onEnter={(node, isAppearing)=>{
+                  console.log('match')
+                  console.log(match)
+                  console.log(show);
+                  console.log('node');
+                  console.log('开始进入。。。');
+                  console.log(node)
+                  console.log(isAppearing)
+                  console.log('isAppearing')
+               }}
+                onEntering={(node, isAppearing) => {
+                  setShow(false)
+                  console.log(show);
+                  console.log('进入中。。。');
+                  console.log('entering11')
+                  console.log('onEntering-node')
+                  console.log(node)
+                  console.log(isAppearing)
+                }}
+                onExit={(node) => {
+                  console.log(show);
+                  console.log(node);
+                  console.log('退出之前')
+                  console.log('onExiting？？？')
+                }}
+                onExiting={(node) => {
+                  setShow(true)
+                  console.log(show);
+                  console.log(node);
+                  console.log('退出中....')
+                  console.log('onExiting222')
+                }}
+                onEntered={(node, isAppearing) => {
+                  console.log(show);
+                  console.log('进入完成....')
+                  console.log(node,isAppearing)
+                  console.log('onEntered3333')
+                }}
+                onExited={(node) => {
+                  console.log(show);
+                  console.log(node);
+                  console.log('退出完成....')
+                  console.log('onExited444')
 
+                }}
+              >
+                <Switch location={location}>{children}</Switch>
+              </CSSTransition>
+            </SwitchTransition>
+          )}
+        />
+      </Spin>
+    </div>
   )
 }
 export default connect(({ loading }) => ({
